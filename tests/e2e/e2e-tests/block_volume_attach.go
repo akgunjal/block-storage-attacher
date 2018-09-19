26ce278dbc3311086307ebadcff471b4984aeb45
Murali <mumutyal@in.ibm.com>
2018-09-19 06:23:35 -0400

    e2e changes

--- tests/e2e/e2e-tests/block_volume_attach.go
+++ tests/e2e/e2e-tests/block_volume_attach.go
@@ -151,8 +151,8 @@ func fileExists(filename string) (bool, error) {
 
 func getAttchStatus() (string, error) {
 	attachStatus := "attaching"
-	pv, err := c.Core().PersistentVolumes().Get(pvname)
-	err := errors.New("Timed out in PV creation")
+	pv, _ = c.Core().PersistentVolumes().Get(pvname)
+	err = errors.New("Timed out in PV creation")
 	for start := time.Now(); time.Since(start) < (5 * time.Minute); {
 		attachStatus = pv.ObjectMeta.Annotations["ibm.io/attachstatus"]
 		fmt.Printf("attachStatus :\n%s\n", attachStatus)
